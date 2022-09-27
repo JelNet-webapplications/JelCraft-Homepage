0042ed4719d118a73e52d5723511398a4c785de4
vivaloz <olafjanse10@gmail.com>
2022-09-27 15:54:09 +0200

    revamped doTranslation, the page can translate properly

--- Site/script/translations/translate.js
+++ Site/script/translations/translate.js
@@ -1,12 +1,8 @@
 function doTranslation(lan){
     setFlagBrightness(lan);
     for(temp in translations){
-        console.log("%ctranslated element: %c"+temp,"color:orange;","color: lightgray;")
         let id = "Б"+temp, // Б is the identifier used for html elements which need to be translated
-            element = document.getElementById(id);
-        if(element.innerHTML == null) continue;
-        
-        /*
+        element = document.getElementById(id);
         switch(temp){
             case "curtains-projects-void-title":
             case "curtains-projects-void-description":
@@ -14,27 +10,17 @@ function doTranslation(lan){
                     let TBTid = `${id}${i}`;
                     document.getElementById(TBTid).innerHTML = translations[temp][lan];
                 }
-            case "curtains-contact-emailadress":
+                break;
+            case "curtains-contact-emailaddress":
             case "curtains-contact-nickname":
             case "curtains-contact-subject":
             case "curtains-contact-message":
                 element.placeholder = translations[temp][lan];
+                break;
             default:
                 element.innerHTML = translations[temp][lan];
         }
-        */
- 
-        if(temp == "To_be_determined" || temp == "Nonapplicable"){
-            for(let i = 2; i < 6; i++){
-                let TBTid = `${id}${i}`;
-                element.innerHTML = translations[temp][lan];
-            }
-        }
-        else if(temp == 'Email_Address' || temp == 'Nickname' || temp == 'Subject' || temp == 'Message'){
-            element.placeholder = translations[temp][lan];
-        } else {
-            element.innerHTML = translations[temp][lan];
-        }
+        console.log("%ctranslated element in language %c\""+lan+"\"%c: %c"+temp,"color:orange;","color: orangered","color:orange;","color: lightgray;")
     }
 }
 
