22f2ab08ac1f99f66bbf79f198c8cef009e23b33
vivaloz <olafjanse10@gmail.com>
2022-09-27 15:53:25 +0200

    fixed json names

--- Site/script/translations/jsontranslations.js
+++ Site/script/translations/jsontranslations.js
@@ -1,259 +1,259 @@
 const translations = {
     "navbar-jelnet": {
         "en": "JELNET",
-        "de": "",
+        //"de": "",
         "nl": "JELNET",
         "vk": "JELNET"
     },
     "navbar-home": {
         "en": "HOME",
-        "de": "",
+        //"de": "",
         "nl": "THUISPAGINA",
         "vk": "PEGUÊNE ÈTTONOS"
     },
     "navbar-internal": {
         "en": "INTERNAL",
-        "de": "",
+        //"de": "",
         "nl": "INTERN",
         "vk": "INTARANIIR"
     },
     "navbar-external": {
         "en": "EXTERNAL",
-        "de": "",
+        //"de": "",
         "nl": "EXTERN",
         "vk": "TORRANIIR"
     },
     "curtains-projects-title": {
         "en": "<br>Projects",
-        "de": "",
+        //"de": "",
         "nl": "<br>Projecten",
         "vk": "<br>Nvunvanvgarkän"
     },
     "curtains-projects-pinballconverter-title": {
         "en": "Pinball Converter",
-        "de": "",
+        //"de": "",
         "nl": "Pinball-wisselaar",
         "vk": "Vgaûsälerevyo Pënpvëlän"
     },
     "curtains-projects-pinballconverter-description": {
         "en": "<br>Exchange rate EUR - Pinballs",
-        "de": "",
+        //"de": "",
         "nl": "<br>Wisselkoers van EUR - Pinball-snoepjes",
         "vk": "<br>Kûirsevyo èvgaûsäl özruârogüt snûipänen Èpënpvëlän"
     },
     "curtains-projects-sudokusolver-title": {
         "en": "Sudoku Solver",
-        "de": "",
+        //"de": "",
         "nl": "Sudoku-oplosser",
         "vk": "Terûlüsärevyo Sütökuês"
     },
     "curtains-projects-sudokusolver-description": {
         "en": "<br>Automatically solves Sudokus",
-        "de": "",
+        //"de": "",
         "nl": "<br>Lost automatisch Sudoku's op",
         "vk": "<br>Ylüsevt áuêtunvetuêsgvvgarro Sütökuês terû"
     },
     "curtains-projects-worldstats-title": {
         "en": "WorldStats",
-        "de": "",
+        //"de": "",
         "nl": "Wereldstatistieken",
         "vk": "Vsifärenänevyo pvüläkòio"
     },
     "curtains-projects-worldstats-description": {
         "en": "<br>Shows real live-time global statistics",
-        "de": "",
+        //"de": "",
         "nl": "<br>Toont real-time wereldwijde statistieken",
         "vk": "<br>Ygáifëvvgärstëstaûstuêkän èttagvtä èvgaûzrältvvgitvä"
     },
     "curtains-projects-flightcalculator-title": {
         "en": "Flight Calculator",
-        "de": "",
+        //"de": "",
         "nl": "Vluchtcalculator",
         "vk": "Ráikäntvaûnká tygo vluêgärùs èsüzrtvën"
     },
     "curtains-projects-flightcalculator-description": {
         "en": "<br>Calculates efficient Minecraft flight data",
-        "de": "",
+        //"de": "",
         "nl": "<br>Berekent efficiënte Minecraft-vluchtgegevens",
         "vk": "<br>Ypväráikänuvgärgáigävänsevyo èsnalä Náinkreft"
     },
     "curtains-projects-collatzcalculator-title": {
         "en": "Collatz Calculator",
-        "de": "",
+        //"de": "",
         "nl": "Collatz-calculator",
         "vk": "Ráikäntvaûnká tygo värnvûitvänòioevyo Kolats"
     },
     "curtains-projects-collatzcalculator-description": {
         "en": "<br>Calculates everything collatz conjecture",
-        "de": "",
+        //"de": "",
         "nl": "<br>Berekent alles over het Collatz-vermoeden",
         "vk": "<br>Ypväráikänuvgärsëknä klyeron värnûitvänevyo Kolats"
     },
-    "curtains-projects-sbubbygenerator": {
+    "curtains-projects-sbubbygenerator-title": {
         "en": "Sbubby Generator",
-        "de": "",
+        //"de": "",
         "nl": "Sbubby-generator",
         "vk": "Pletenännvekärâtygo Spupiän"
     },
     "curtains-projects-sbubbygenerator-description": {
         "en": "<br>Create your own sbubby of existing logos",
-        "de": "",
+        //"de": "",
         "nl": "<br>Maak je eigen sbubby van bestaande logo's",
         "vk": "<br>Kënkonydünryuêrvgärro Spupiänievyo èttigän nvërktpleten èpvästentvä"
     },
     "curtains-projects-hangmanengine-title": {
         "en": "Hangman Engine",
-        "de": "",
+        //"de": "",
         "nl": "Calculator voor Galgje",
         "vk": "Ráikäntvaûnkáevyo Gëlgen"
     },
     "curtains-projects-hangmanengine-description": {
         "en": "<br>Calculate the perfect guess for hangman",
-        "de": "",
+        //"de": "",
         "nl": "<br>Berekent de perfecte gok voor Galgje",
         "vk": "<br>Ypväráikänevvgärgükòio èparfactä tygo Gëlgen"
     },
     "curtains-projects-wordfeudcalculator-title": {
         "en": "Wordfeud Calculator",
-        "de": "",
+        //"de": "",
         "nl": "Wordfeud-calculator",
         "vk": "Ráikäntvaûnkáevyo Vgüzrtvspalen"
     },
     "curtains-projects-wordfeudcalculator-description": {
         "en": "<br>Calculate the best words for Wordfeud and Scrabble",
-        "de": "",
+        //"de": "",
         "nl": "<br>Berekent de beste woorden voor Wordfeud en Scrabble",
         "vk": "<br>Ypväráikänevvgärvgürztvänòio èpvastä tygo Vgüzrtvspalen ant Skrepûl"
     },
     "curtains-projects-dirkmap-title": {
         "en": "Dirk Map",
-        "de": "",
+        //"de": "",
         "nl": "Dirk-plattegrond",
         "vk": "Grüntvevyo ènyspikäzg Tërk"
     },
     "curtains-projects-dirkmap-description": {
         "en": "<br>Shows the perfect route to find your groceries",
-        "de": "",
+        //"de": "",
         "nl": "<br>Toont de beste route om je boodschappen te vinden",
         "vk": "<br>Ygáifevvgärrûitäòio èpvastä yost pvutsgvëpäni vaûntvänä"
     },
     "curtains-projects-starlabsdelivery-title": {
         "en": "Starlabs Delivery",
-        "de": "",
+        //"de": "",
         "nl": "Starlabs-bezorging",
         "vk": "Pvösgáinvaûnkáevyo Starleps"
     },
     "curtains-projects-starlabsdelivery-description": {
         "en": "<br>Order blocks online for the Minecraft H43 Server",
-        "de": "",
+        //"de": "",
         "nl": "<br>Bestel digitale blokken voor de Minecraft H43-server",
         "vk": "<br>Yost kopvüsänen èttái tygo Náinkraft Ry43 tvûinär pvästalänä"
     },
     "curtains-projects-viviolasktranslator-title": {
-        "en": "",
-        "de": "",
-        "nl": "",
-        "vk": ""
+        "en": "Viviolask Translator",
+        //"de": "",
+        "nl": "Viviolask-vertaler",
+        "vk": "Telyostsvatäruyo Viviöläzk"
     },
     "curtains-projects-viviolasktranslator-description": {
-        "en": "",
-        "de": "",
-        "nl": "",
-        "vk": ""
+        "en": "<br>Translate Dutch into the conlang we call Viviolask",
+        //"de": "",
+        "nl": "<br>Vertaal Nederlands in de contaal die we Viviolask noemen",
+        "vk": "<br>Kënivgarnáitvärlëntvsâno värtelänë raniir kvüntelòio svinyra nûinvonvgëdälryatkürviviöläzk"
     },
     "curtains-projects-void-title": {
         "en": "To be determined",
-        "de": "",
+        //"de": "",
         "nl": "Nader te bepalen",
-        "vk": "Tverne kuêsvänä"
+        "vk": "Tveravyt kuêsvänä"
     },
     "curtains-projects-void-description": {
         "en": "<br>Non-applicable",
-        "de": "",
+        //"de": "",
         "nl": "<br>Niet van toepassing",
         "vk": "<br>Nynyevyo gäpvrûik"
     },
     "curtains-aboutus-title": {
         "en": "<br>About us",
-        "de": "",
+        //"de": "",
         "nl": "<br>Over ons",
         "vk": "<br>Klyeron vgiozok"
     },
     "curtains-aboutus-jelcraft-title": {
         "en": "JelCraft",
-        "de": "",
+        //"de": "",
         "nl": "JelCraft",
         "vk": "Jelkraft"
     },
     /*"curtains-aboutus-jelcraft-description-old": {
         "en": "<br>Proud father of two",
-        "de": "",
+        //"de": "",
         "nl": "<br>Trotse vader van 2 kinderen",
         "vk": "<br>Vetvärevyo èpvliä kaûntväränenù ètvgái"
     },*/
     "curtains-aboutus-jelcraft-description": {
         "en": "<br>50 percent digital genius, 50 percent total idiot",
-        "de": "",
+        //"de": "",
         "nl": "<br>50 procent digitaal genie, 50 percent totale idioot",
         "vk": "<br>Tygoâprusvänt èviftäzg svyänúei èttái, tygoâprusvänt èviftäzg kuêrävguêtä èvuláitväzg"
     },
     "curtains-aboutus-vivaloz-title": {
         "en": "Vivaloz",
-        "de": "",
+        //"de": "",
         "nl": "Vivaloz",
         "vk": "Viväloz"
     },
     "curtains-aboutus-vivaloz-description": {
         "en": "<br>Absolutely lunatic programmer who codes some stuff sometimes for fun",
-        "de": "",
+        //"de": "",
         "nl": "<br>Krankzinnige programmeur die af en toe dingen codeert voor de lol",
         "vk": "<br>Kvünpyûiztärùnvënnvekär èsvuêktänvekäntvä svinyra sünvsantsvu tvaûnkáyän kvutvaûzrev tygo pläsvuêròio"
     },
     "curtains-contact-title": {
         "en": "<br>Contact",
-        "de": "",
+        //"de": "",
         "nl": "<br>Contact",
         "vk": "<br>Eûtrikänë allün vgiozok"
     },
-    "curtains-contact-email": {
+    "curtains-contact-emailaddress": {
         "en": "Email Address",
-        "de": "",
+        //"de": "",
         "nl": "Email-adres",
         "vk": "Pletsnenvi èttáipvruêf"
     },
     "curtains-contact-nickname": {
         "en": "Nickname",
-        "de": "",
+        //"de": "",
         "nl": "Gebruikersnaam",
         "vk": "Nenvi èttaûntärpvine"
     },
     "curtains-contact-subject": {
         "en": "Subject",
-        "de": "",
+        //"de": "",
         "nl": "Onderwerp",
-        "vk": "Terûgüzjì"
+        "vk": "Terûgüzyì"
     },
     "curtains-contact-message": {
         "en": "Message",
-        "de": "",
+        //"de": "",
         "nl": "Bericht",
         "vk": "Vgët vgaûlko svagänë"
     },
     "curtains-contact-send": {
         "en": "Send",
-        "de": "",
+        //"de": "",
         "nl": "Versturen",
         "vk": "Terûstoränë"
     },
     "curtains-discordserver-title": {
         "en": "<br>Discord server",
-        "de": "",
+        //"de": "",
         "nl": "<br>Discord-server",
         "vk": "<br>Pretkenelevyo Tëskorton"
     },
     // "": {
     //     "en": "",
-    //     "de": "",
+    //     //"de": "",
     //     "nl": "",
     //     "vk": ""
     // },
