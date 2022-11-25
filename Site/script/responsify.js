2d4554f8a465d4922830fb12b2b6cdfa1e4e7971
JelCraft <Jelcraft@outlook.com>
2022-11-25 12:29:24 +0100

    Alles is nu responsive

--- Site/script/responsify.js
+++ Site/script/responsify.js
@@ -1,9 +1,16 @@
 function responsify(){
 	let wwidth = window.innerWidth;
 	if(wwidth < 1200){
-		// complete teringbende naar beneden onder elkaar
-	} 
-	else if(wwidth < 1400) {
-		//curtains ietsje langer
+		document.getElementById('layout-container').style.gridTemplateColumns = '80%'
+		document.getElementById('projects').style.gridRow = '1'
+		document.getElementById('discord-server').style.gridRow = '4'
+	} else {
+		document.getElementById('layout-container').style.gridTemplateColumns = '40% 40%'
+		document.getElementById('projects').style.gridRow = '1 / 20'
+		document.getElementById('discord-server').style.gridRow = '3 / 20'
 	}
+
+	// else if(wwidth < 1400) {
+	// 	//curtains ietsje langer
+	// }
 }
