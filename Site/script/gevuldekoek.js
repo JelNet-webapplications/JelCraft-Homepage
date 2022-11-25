a8139f4c14fd6380ef2e5a882596977563ed60c7
JelCraft <Jelcraft@outlook.com>
2022-11-25 12:13:21 +0100

    Fixed #50

--- Site/script/gevuldekoek.js
+++ Site/script/gevuldekoek.js
@@ -1,8 +1,8 @@
 function dTsC(){
     // checkCookie();
-    doTranslation('en');
     setInterval(responsify, 1000);
     loadPref();
+    doTranslation('en');
     document.getElementsByClassName('navbar').style.transition = 'all 0s ease 0s';
     console.log('%cthe following launch functions have been called succesfully:%c\n\t"doTranslation(\'en\')"\n\t"setInterval(responsify, 1000)"',"color: darkcyan; font-family: \"Comic Sans MS\", \"Comic Sans\", cursive;","color: lightgrey; font-family:\"Comic Sans MS\", \"Comic Sans\", cursive;")
 }
