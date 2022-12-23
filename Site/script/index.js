e791fc000956d6b89ae748ac4b33948f74a2af7e
vivaloz <olafjanse10@gmail.com>
2022-12-23 22:46:40 +0100

    issue #16 verholpen

--- Site/script/index.js
+++ Site/script/index.js
@@ -1,12 +1,19 @@
 addEventListener('DOMContentLoaded', (event) => {
-    WIPAlert()
+    WIPAlert("popup");
 });
 
-function WIPAlert() {
+function WIPAlert(typeOfAlert) {
     let WIP = document.getElementsByClassName("link-wip");
     for(let i=0;i<WIP.length;i++){
         WIP[i].addEventListener("click", () => {
-            alert("This project is still under development! We're working on it, try again in the near future.");
+            if(typeOfAlert == "alert"){
+                alert("This project is still under development! We're working on it, try again in the near future.");
+            } else if(typeOfAlert == "popup"){
+                document.getElementById("wippopup").style.left = "-10px";
+                setTimeout(() => {
+                    document.getElementById("wippopup").style.left = "-410px";
+                  }, "5000")
+            }
         });
     }
 }
